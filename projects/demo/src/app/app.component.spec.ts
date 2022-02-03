779e28ff493ef607a27d58cc50169e0edaaa213e
w3soto <soto.ms@gmail.com>
2022-02-03 14:06:38 +0100

    Add missing imports to demo app test

--- projects/demo/src/app/app.component.spec.ts
+++ projects/demo/src/app/app.component.spec.ts
@@ -1,5 +1,20 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { BrowserModule } from "@angular/platform-browser";
+import { NoopAnimationsModule } from "@angular/platform-browser/animations";
+import { FormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
+import { MatButtonModule } from "@angular/material/button";
+import { MatButtonToggleModule } from "@angular/material/button-toggle";
+import { MatRadioModule } from "@angular/material/radio";
+import { MatExpansionModule } from "@angular/material/expansion";
+import { MatCheckboxModule } from "@angular/material/checkbox";
+import { MatCardModule } from "@angular/material/card";
+import { MatIconModule } from "@angular/material/icon";
+import { MatDividerModule } from "@angular/material/divider";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatTabsModule } from "@angular/material/tabs";
+import { NgxMatTimelineModule } from "../../../ngx-mat-timeline/src/lib/ngx-mat-timeline.module";
 
 describe('AppComponent', () => {
   beforeEach(async () => {
@@ -7,6 +22,25 @@ describe('AppComponent', () => {
       declarations: [
         AppComponent
       ],
+      imports: [
+        BrowserModule,
+        NoopAnimationsModule,
+        FormsModule,
+        HttpClientModule,
+
+        MatButtonModule,
+        MatButtonToggleModule,
+        MatRadioModule,
+        MatExpansionModule,
+        MatCheckboxModule,
+        MatCardModule,
+        MatIconModule,
+        MatDividerModule,
+        MatToolbarModule,
+        MatTabsModule,
+
+        NgxMatTimelineModule
+      ]
     }).compileComponents();
   });
 
